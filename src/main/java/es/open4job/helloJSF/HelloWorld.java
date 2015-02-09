c175fe19d752b2b73a5c1bda1e8980f90115bac8
alariel91 <monysaenzcarrero@gmail.com>
2015-02-09 11:18:29 +0100

    COnversion fecha , la fecha tiene que ser un Date

--- src/main/java/es/open4job/helloJSF/HelloWorld.java
+++ src/main/java/es/open4job/helloJSF/HelloWorld.java
@@ -1,6 +1,7 @@
 package es.open4job.helloJSF;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -13,19 +14,20 @@ public class HelloWorld implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	private String nombre;
-	private String date;
+	private Date date;
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String getDate() {
+	public Date getDate() {
 		return date;
 	}
-	public void setDate(String date) {
+	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	
 	 
 	
