2e452c33fdf625751dd216b5eb6ceb965fc1138c
alariel91 <monysaenzcarrero@gmail.com>
2015-02-09 10:47:22 +0100

    Un hola mundo con JSF , un formulario en el que introduces un nombre y te saluda.

---
+++ src/main/java/es/open4job/helloJSF/HelloWorld.java
@@ -0,0 +1,25 @@
+package es.open4job.helloJSF;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+@ManagedBean
+@SessionScoped
+public class HelloWorld implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	 
+	
+
+}
