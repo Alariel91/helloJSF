de5a5711a5dc096816b0b6db344700e2c799359e
alariel91 <monysaenzcarrero@gmail.com>
2015-02-09 11:04:53 +0100

    Agregado un campo fecha con su validacion

--- src/main/java/es/open4job/helloJSF/HelloWorld.java
+++ src/main/java/es/open4job/helloJSF/HelloWorld.java
@@ -13,12 +13,20 @@ public class HelloWorld implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	private String nombre;
+	private String date;
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
 	 
 	
 
